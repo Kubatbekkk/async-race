62b936e79b39ee858253f29f6275b0bca3c2798d
Kubatbekkk <kubineccastro@gmail.com>
2023-01-21 13:57:10 +0600

    feat: add cars view, fix disabled buttons

--- src/components/header.ts
+++ src/components/header.ts
@@ -8,19 +8,10 @@ export class Header implements HeaderModel {
         <header class="header-wrapper">
             <div class="header-container">
                 <h1 class="header-container__title" id="app-title">
-                    Need for Promise
+                    async race
                 </h1>
                 <div class="header-container__items">
-                  <div class="audio-payer">
-                    <audio class="audio" id="audio"></audio>
-                    <div class="controls">
-                      <div id="prev" class="audio-payer__btn prev"></div>
-                      <div id="play" class="audio-payer__btn play"></div>
-                      <div id="next" class="audio-payer__btn next"></div>
-                    </div>
-                    <progress class="progress-bar" id="progress-bar" value="0" max="1"></progress>
-                    <div id="mute" class="audio-payer__btn mute"></div>
-                  </div>
+
                   <nav class="header-nav">
                     <div class="header-nav__item">
                         <button id="garage-btn" class="header__btn button">
